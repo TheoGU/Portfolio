8b60da5e9e24eed3b8cd923c0887f3439f46b495
Guinebertier <theo@guinebertier.com>
2019-02-21 23:18:45 +0100

    project Case done

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './style.css'
 
 const Project = (props) => {
   return (
     <div style={styles.container}>
-      <h1>Project</h1>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
+      <div className="block" style={styles.block}><p>ok</p></div>
     </div>
   )
 }
@@ -12,8 +22,20 @@ export default Project
 
 const styles = {
   container:{
+    display:'flex',
+    flexWrap: 'wrap',
     height: `100vh`,
-    border: `1px solid #55efc4`
-
+    border: `1px solid #55efc4`,
+    justifyContent:'space-around',
+    alignItems:'center',
+    alignContent:'center',
+    paddingLeft:50,
+    paddingRight:50,
+  },
+  block:{
+    width:200,
+    height:200,
+    border: `1px solid orange`,
+    margin:10
   }
 }
